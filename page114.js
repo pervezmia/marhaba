efcb613bfcd5d31415d2f9570934c62dc3a98085
pervezmia <ahmedpervezkabir@gmail.com>
2025-09-16 16:12:09 +0600

    trying commit

---
+++ page114.js
@@ -0,0 +1,20 @@
+        // practice no.1
+
+for (let num = 150; num<=170; num++){
+    console.log(num);
+}
+
+        // practice no.2
+    let sum = 0;
+for (let roll=31; roll<=58; roll++){
+    console.log(roll);
+    sum = sum + roll;
+}
+    console.log(sum);
+
+            // practice no.3
+        let totalNumber = 0;
+    for (number=25; number<=75; number++){
+        totalNumber = totalNumber + number;
+    }
+    console.log(totalNumber);
