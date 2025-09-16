b9b758dccfc9efb098d96669e260af9e3fc251b0
pervezmia <ahmedpervezkabir@gmail.com>
2025-09-16 16:00:12 +0600

    try to practice marhaba

---
+++ page109.js
@@ -0,0 +1,41 @@
+    // practice no. 1 
+    let coding = 1 ;
+    while ( coding <= 10){
+        console.log("i will be code minumun three hours in per day");
+        coding = coding + 1;    
+    }
+
+
+    // practice no. 2
+
+    let number = 1;
+    while ( number <= 20){
+        console.log(number);
+        number = number + 1;
+    }
+
+    // practice no. 3
+
+    let num = 50;
+    while (num <=100){
+        console.log(num);
+        num =num+1;
+    }
+
+    // practice no. 4
+
+    let namb = 1;
+    while (namb <=10){
+        let namota = namb * 3;
+        console.log(namota);
+        namb = namb + 1;
+    }
+
+    // practice no. 5
+    let n = 111 ;
+    while ( n >= 110){
+        let vag = n/2;
+        console.log(vag);
+        n = n-1
+    }
+    
