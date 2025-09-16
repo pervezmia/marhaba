7f0ada8f5c8b82065d98436cdce19e10e83ab535
pervezmia <ahmedpervezkabir@gmail.com>
2025-09-16 16:06:07 +0600

    this commit is second file

---
+++ page111.js
@@ -0,0 +1,50 @@
+        // practice no 1
+
+            let num = 50;
+            while (num<=100){
+                console.log(num);
+                num++
+            }
+
+
+        // practice no 2
+            let number = 5;
+            let sum = 0;
+            while (number<=15){
+                console.log(number);
+                sum = sum + number;
+                number++;
+                
+                
+            }
+            console.log(sum);
+
+        // practice no.3
+        let roll = 1;
+        let total = 0;
+        while (roll <= 50){
+            console.log(roll);
+            roll++;
+            total = total + roll;
+        }
+        console.log(total);
+
+        // practice no.4
+        let numb1 = 21;
+        let sum1 = 0;
+        while (numb1 <=40){            
+            sum1 = sum1 + numb1;
+            numb1++;
+            console.log(sum1)
+        }    
+
+        // practice no.5
+        let firstDigit = 20;
+        let totall = 0;
+        while (firstDigit<=40){
+            totall = totall + firstDigit;
+            firstDigit++;
+        }
+        console.log(totall);
+
+        
